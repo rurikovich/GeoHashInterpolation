ae22c7af517bacc586fd3a076127067407473e69
rurik <urrast@yandex.ru>
2016-11-29 22:05:50 +0300

    GeohashGrid implementation

--- src/main/java/org/rurik/geohash/GeohashGrid.java
+++ src/main/java/org/rurik/geohash/GeohashGrid.java
@@ -1,34 +1,63 @@
 package org.rurik.geohash;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Math.*;
+
 /**
  * Created by Yuri Rastegaev on 27.11.2016.
  */
 public class GeohashGrid {
+
+    private final int LONGITUDE_PERIOD = 360;
+    private final int LONGITUDE_CORRECTION = -180;
+
+    private final int LATITUDE_PERIOD = 180;
+    private final int LATITUDE_CORRECTION = -90;
+
     /**
      * Geohash precision is a length of the encoded string: precision of the "bc3d" is 4.
      **/
     private int precision;
-    private double[] latitudes;
-    private double[] longitudes;
+    private List<Double> latitudes;
+    private List<Double> longitudes;
 
     public GeohashGrid(int precision) {
         this.precision = precision;
-        calculateGrid(precision);
+        calculateGrid();
     }
 
-    private void calculateGrid(int precision) {
-//        TODO
+    private void calculateGrid() {
+        latitudes = calculateLatitudes(precision);
+        longitudes = calculateLongitudes(precision);
+    }
+
+    private List<Double> calculateLatitudes(int precision) {
+        return generateCoordinates(LATITUDE_PERIOD / pow(2, precision), LATITUDE_PERIOD, LATITUDE_CORRECTION);
+    }
+
+    private List<Double> calculateLongitudes(int precision) {
+        return generateCoordinates(LONGITUDE_PERIOD / pow(2, precision + 1), LONGITUDE_PERIOD, LONGITUDE_CORRECTION);
+    }
+
+    private List<Double> generateCoordinates(double step, int period, int correction) {
+        List<Double> coordinates = new ArrayList<>();
+        for (double l = 0; l <= period; l += step) {
+            coordinates.add(l + correction);
+        }
+        return coordinates;
     }
 
     public int getPrecision() {
         return precision;
     }
 
-    public double[] getLatitudes() {
+    public List<Double> getLatitudes() {
         return latitudes;
     }
 
-    public double[] getLongitudes() {
+    public List<Double> getLongitudes() {
         return longitudes;
     }
 }
