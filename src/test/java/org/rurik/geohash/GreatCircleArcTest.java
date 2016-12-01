ea4b310a3c853c801859a87a8285df97a53be991
rurik <urrast@yandex.ru>
2016-12-01 16:08:11 +0300

    GreatCircleArcTest

---
+++ src/test/java/org/rurik/geohash/GreatCircleArcTest.java
@@ -0,0 +1,21 @@
+package org.rurik.geohash;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by rurik on 01.12.2016.
+ */
+public class GreatCircleArcTest {
+    @Test
+    public void computeLatitude() throws Exception {
+
+    }
+
+    @Test
+    public void computeLongitude() throws Exception {
+
+    }
+
+}
