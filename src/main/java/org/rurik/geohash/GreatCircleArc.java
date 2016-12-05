91183f486daf7a8cb9fa8e8946eae4621afdb6a4
rurik <urrast@yandex.ru>
2016-12-05 17:03:24 +0300

    GreatCircleArcTest: fix1 radians

--- src/main/java/org/rurik/geohash/GreatCircleArc.java
+++ src/main/java/org/rurik/geohash/GreatCircleArc.java
@@ -6,57 +6,66 @@ import static java.lang.Math.*;
  * Created by Yuri Rastegaev on 27.11.2016.
  */
 public class GreatCircleArc {
-    private double latitude1;
-    private double longitude1;
+    private double latitudeInRad1;
+    private double longitudeInRad1;
 
-    private double latitude2;
-    private double longitude2;
+    private double latitudeInRad2;
+    private double longitudeInRad2;
 
     private double sin_diff_lon2_lon1;
     private double tan_lat1;
     private double tan_lat2;
 
+    private double A;
+    private double B;
+
     private double sqrt_sum_A2_B2;
     private double A_div_sqrt_sum_A2_B2;
 
-
     public GreatCircleArc(GeoPoint geoPoint1, GeoPoint geoPoint2) {
-        latitude1 = geoPoint1.getLatitude();
-        longitude1 = geoPoint1.getLongitude();
-        latitude2 = geoPoint2.getLatitude();
-        longitude2 = geoPoint2.getLongitude();
-        preComputeConstants(latitude1, longitude1, latitude2, longitude2);
+        this(geoPoint1.getLatitude(), geoPoint1.getLongitude(), geoPoint2.getLatitude(), geoPoint2.getLongitude());
     }
 
     public GreatCircleArc(double latitude1, double longitude1, double latitude2, double longitude2) {
-        this.latitude1 = latitude1;
-        this.longitude1 = longitude1;
-        this.latitude2 = latitude2;
-        this.longitude2 = longitude2;
-        preComputeConstants(latitude1, longitude1, latitude2, longitude2);
+        latitudeInRad1 = getRadian(latitude1);
+        longitudeInRad1 = getRadian(longitude1);
+        latitudeInRad2 = getRadian(latitude2);
+        longitudeInRad2 = getRadian(longitude2);
+        preComputeConstants(latitudeInRad1, longitudeInRad1, latitudeInRad2, longitudeInRad2);
     }
 
-    private void preComputeConstants(double latitude1, double longitude1, double latitude2, double longitude2) {
-        sin_diff_lon2_lon1 = sin(this.longitude2 - this.longitude1);
-        tan_lat1 = tan(this.latitude1);
-        tan_lat2 = tan(this.latitude2);
+    public Double computeLatitude(Double longitude) {
+        double longitudeRad = getRadian(longitude);
+        double exp1 = tan_lat1 * sin(longitudeInRad2 - longitudeRad) / sin_diff_lon2_lon1;
+        double exp2 = tan_lat2 * sin(longitudeRad - longitudeInRad1) / sin_diff_lon2_lon1;
+        return getAngle(atan(exp1 + exp2));
+    }
 
-        double A = tan_lat2 * cos(this.longitude1) - tan_lat1 * cos(this.longitude2);
-        double B = tan_lat1 * sin(this.longitude2) - tan_lat2 * sin(this.longitude1);
+    public Double computeLongitude(Double latitude) {
+        double latitudeRad = getRadian(latitude);
+        double C = tan(latitudeRad) * sin_diff_lon2_lon1;
+        return getAngle(-acos(A_div_sqrt_sum_A2_B2) + asin(C / sqrt_sum_A2_B2));
+    }
+
+    private void preComputeConstants(double latitudeInRad1, double longitudeInRad1, double latitudeInRad2, double longitudeInRad2) {
+        sin_diff_lon2_lon1 = sin(longitudeInRad2 - longitudeInRad1);
+        tan_lat1 = tan(latitudeInRad1);
+        tan_lat2 = tan(latitudeInRad2);
+
+        A = tan_lat2 * cos(longitudeInRad1) - tan_lat1 * cos(longitudeInRad2);
+        B = tan_lat1 * sin(longitudeInRad2) - tan_lat2 * sin(longitudeInRad1);
 
         sqrt_sum_A2_B2 = sqrt(A * A + B * B);
         A_div_sqrt_sum_A2_B2 = A / sqrt_sum_A2_B2;
 
     }
 
-    public Double computeLatitude(Double longitude) {
-        double exp1 = tan_lat1 * sin(longitude2 - longitude) / sin_diff_lon2_lon1;
-        double exp2 = tan_lat2 * sin(longitude - longitude1) / sin_diff_lon2_lon1;
-        return atan(exp1 + exp2);
+    private Double getRadian(Double angle) {
+        return angle * (PI / 180);
     }
 
-    public Double computeLongitude(Double latitude) {
-        double C = tan(latitude) * sin_diff_lon2_lon1;
-        return -acos(A_div_sqrt_sum_A2_B2) + asin(C / sqrt_sum_A2_B2);
+    private Double getAngle(Double radian) {
+        return radian * (180 / PI);
     }
+
 }
--- src/test/java/org/rurik/geohash/GreatCircleArcTest.java
+++ src/test/java/org/rurik/geohash/GreatCircleArcTest.java
@@ -8,13 +8,14 @@ import static org.junit.Assert.assertEquals;
  * Created by rurik on 01.12.2016.
  */
 public class GreatCircleArcTest {
-    private static final Integer STEPS_NUMBER = 1;
-    private static final Double DELTA = 0.00000000475173;
-    private static final Double STEP = DELTA / STEPS_NUMBER;
-
-    private static final Double POINT_LONGITUDE = 46.00790977478027;
-    private static final Double POINT_LATITUDE = 51.52337735475173;
     public static final double DOUBLE_DELTA = 0.00000000001;
+
+    private static final Double POINT_LONGITUDE = 45d;
+    private static final Double POINT_LATITUDE = 45d;
+    private static final Double DELTA = 1d;
+
+    private static final Integer STEPS_NUMBER = 1;
+    private static final Double STEP = DELTA / STEPS_NUMBER;
 
     @Test
     public void constantLongitudeArc() throws Exception {
@@ -23,8 +24,9 @@ public class GreatCircleArcTest {
         Double latEnd = POINT_LATITUDE + DELTA;
 
         GreatCircleArc arc = new GreatCircleArc(latStart, longitude, latEnd, longitude);
-        for (double lat = latStart; lat <= latEnd; lat += STEP) {
-            assertEquals(longitude, arc.computeLongitude(lat));
+        for (Double lat = latStart; lat <= latEnd; lat += STEP) {
+            Double lon = arc.computeLongitude(lat);
+            assertEquals(longitude, lon);
         }
     }
 
@@ -34,7 +36,7 @@ public class GreatCircleArcTest {
         Double latitude = 0d;
         Double lonStart = -45d;
         Double lonEnd = 45d;
-        double step = 0.5;
+        Double step = 0.5;
 
         GreatCircleArc arc = new GreatCircleArc(latitude, lonStart, latitude, lonEnd);
         for (double lon = lonStart; lon <= lonEnd; lon += step) {
