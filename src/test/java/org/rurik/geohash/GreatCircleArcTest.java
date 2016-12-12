b2f026d9b37a040da63932d0b79ee0e4e7ad8053
rurik <urrast@yandex.ru>
2016-12-12 14:23:26 +0300

    checkConstantLongitudeArc and checkOrdinaryArc metods

--- src/test/java/org/rurik/geohash/GreatCircleArcTest.java
+++ src/test/java/org/rurik/geohash/GreatCircleArcTest.java
@@ -10,35 +10,27 @@ import static org.junit.Assert.assertEquals;
 public class GreatCircleArcTest {
     public static final double DOUBLE_DELTA = 0.00000000001;
 
-
-    @Test
-    public void constantLongitudeArcTest() throws Exception {
-        Double longitude = 40.0083001d;
-        Double latStart = -89d;
-        Double latEnd = longitude + 170d;
-        int step = 10000;
-
-        GreatCircleArc arc = new GreatCircleArc(latStart, longitude, latEnd, longitude);
-        for (Double lat = latStart; lat <= latEnd; lat += step) {
-            Double lon = arc.computeLongitude(lat);
-            assertEquals(longitude, lon, DOUBLE_DELTA);
-        }
-    }
-
-
     @Test
     public void ordinaryArcTest() throws Exception {
-        Double lon1 = 0d;
-        Double lat1 = 7d;
+        checkOrdinaryArc(0d, 7d, 89d, 89d);
+    }
 
-        Double lon2 = 89d;
-        Double lat2 = 89d;
+    private void checkOrdinaryArc(Double lon1, Double lat1, Double lon2, Double lat2) {
         GreatCircleArc arc = new GreatCircleArc(lon1, lat1, lon2, lat2);
         for (double lon = lon1; lon < lon2; lon += 1) {
             Double lat = arc.computeLatitude(lon);
             Double computedLongitude = arc.computeLongitude(lat);
             assertEquals(lon, computedLongitude, DOUBLE_DELTA);
         }
+    }
+
+    @Test
+    public void constantLongitudeArcTest() throws Exception {
+        int steps = 1000;
+        checkConstantLongitudeArc(40.0083001d, -89d, 170d, steps);
+        checkConstantLongitudeArc(0d, 19d, 71, steps);
+        checkConstantLongitudeArc(-77d, -89d, 10d, steps);
+        checkConstantLongitudeArc(-147d, -90d, 180d, 2);
     }
 
     @Test
@@ -51,6 +43,16 @@ public class GreatCircleArcTest {
         GreatCircleArc arc = new GreatCircleArc(latitude, lonStart, latitude, lonEnd);
         for (double lon = lonStart; lon <= lonEnd; lon += step) {
             assertEquals(latitude, arc.computeLatitude(lon), DOUBLE_DELTA);
+        }
+    }
+
+    private void checkConstantLongitudeArc(double longitude, double latStart, double latDelta, int steps) {
+        Double latEnd = latStart + latDelta;
+        GreatCircleArc arc = new GreatCircleArc(latStart, longitude, latEnd, longitude);
+        Double step = latDelta / steps;
+        for (Double lat = latStart; lat <= latEnd; lat += step) {
+            Double lon = arc.computeLongitude(lat);
+            assertEquals(longitude, lon, DOUBLE_DELTA);
         }
     }
 }
